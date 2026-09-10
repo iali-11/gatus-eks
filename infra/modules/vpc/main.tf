913b469b9f931eb64a98a1072bd1e544b96a4814
iali-11 <ibrahima_11@outlook.com>
2026-09-10 23:38:37 +0100

    feat: add vpc module config

---
+++ infra/modules/vpc/main.tf
@@ -0,0 +1,108 @@
+resource "aws_vpc" "vpc" {
+  cidr_block = var.vpc_cidr_block
+
+  tags = {
+    Name = "${var.project-name}-vpc"
+  }
+}
+
+resource "aws_subnet" "public_subnets" {
+  count = 2
+
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.public_subnet_cidr_blocks[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    Name = "${var.project-name}-public-subnet-${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "private_subnets" {
+  count = 2
+
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.private_subnet_cidr_blocks[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    Name = "${var.project-name}-private-subnet-${count.index + 1}"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "${var.project-name}-igw"
+  }
+}
+
+resource "aws_eip" "eip" {
+  domain = "vpc"
+
+  tags = {
+    Name = "${var.project-name}-eip"
+  }
+}
+
+resource "aws_nat_gateway" "natgw" {
+  allocation_id = aws_eip.eip.id
+  subnet_id     = aws_subnet.public_subnets[0].id
+
+  tags = {
+    Name = "${var.project-name}-natgw"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = var.vpc_cidr_block
+    gateway_id = "local"
+  }
+
+  route {
+    cidr_block = var.default_cidr_block
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.project-name}-public-route-table"
+  }
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = var.vpc_cidr_block
+    gateway_id = "local"
+  }
+
+  route {
+    cidr_block = var.default_cidr_block
+    gateway_id = aws_nat_gateway.natgw.id
+  }
+
+  tags = {
+    Name = "${var.project-name}-private-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count = 2
+
+  subnet_id      = aws_subnet.public_subnets[count.index].id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "private" {
+  count = 2
+
+  subnet_id      = aws_subnet.private_subnets[count.index].id
+  route_table_id = aws_route_table.private_route_table.id
+}
