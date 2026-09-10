301ba6024bf05dbb9ca03c32a2ac0ee2e7234f8c
iali-11 <ibrahima_11@outlook.com>
2026-09-10 18:53:37 +0100

    feat: bootstrap root main

---
+++ bootstrap/main.tf
@@ -0,0 +1,14 @@
+module "ecr" {
+  source = "./modules/ecr"
+  image-name = var.image-name
+  image-mutability = var.image-mutability
+}
+
+module "s3" {
+  source = "./modules/s3"
+  bucket-name = var.bucket-name
+  bucket-name-tag = var.bucket-name-tag
+  object_ownership = var.object_ownership
+  sse_algorithm = var.sse_algorithm
+  versioning_configuration = var.versioning_configuration
+}
