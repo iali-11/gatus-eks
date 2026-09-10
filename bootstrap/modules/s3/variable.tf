eb266bc354fe5a8bc409119b7bd4b9aaad2cf8b6
iali-11 <ibrahima_11@outlook.com>
2026-09-10 18:51:13 +0100

    feat: s3 bucket variable values

---
+++ bootstrap/modules/s3/variable.tf
@@ -0,0 +1,19 @@
+variable "bucket-name" {
+  type = string
+}
+
+variable "bucket-name-tag" {
+  type = string
+}
+
+variable "object_ownership" {
+  type = string
+}
+
+variable "versioning_configuration" {
+  type = string
+}
+
+variable "sse_algorithm" {
+  type = string
+}
