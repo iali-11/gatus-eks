d6906aa223ed797c83246a8455f85d0dc344fda9
iali-11 <ibrahima_11@outlook.com>
2026-09-10 23:39:39 +0100

    feat: add vpc module variable values

---
+++ infra/modules/vpc/variable.tf
@@ -0,0 +1,27 @@
+variable "project-name" {
+  type = string
+  description = "Name of the project"
+}
+
+variable "vpc_cidr_block" {
+  type        = string
+  description = "CIDR block of VPC"
+}
+
+variable "public_subnet_cidr_blocks" {
+  type        = list(string)
+  description = "CIDR block of public subnets"
+}
+
+variable "private_subnet_cidr_blocks" {
+  type        = list(string)
+  description = "CIDR block of private subnets"
+}
+
+variable "availability_zones" {
+  type = list(string)
+}
+
+variable "default_cidr_block" {
+  type = string
+}
