39bc8939f9dc47ba97750e15b6330e0a9ecd9d37
iali-11 <ibrahima_11@outlook.com>
2026-09-22 21:19:38 +0100

    fix: terraform format correction

--- bootstrap/variable.tf
+++ bootstrap/variable.tf
@@ -1,36 +1,36 @@
 # ECR Variables
 variable "image-name" {
   default = "gatus"
-  type = string
+  type    = string
 }
 
 variable "image-mutability" {
   default = "IMMUTABLE"
-  type = string
+  type    = string
 }
 
 # S3 Variables
 variable "bucket-name" {
   default = "gatus-eks-backend"
-  type = string
+  type    = string
 }
 
 variable "bucket-name-tag" {
   default = "gatus-eks-s3-bucket"
-  type = string
+  type    = string
 }
 
 variable "object_ownership" {
   default = "BucketOwnerEnforced"
-  type = string
+  type    = string
 }
 
 variable "versioning_configuration" {
   default = "Enabled"
-  type = string
+  type    = string
 }
 
 variable "sse_algorithm" {
   default = "AES256"
-  type = string
+  type    = string
 }
