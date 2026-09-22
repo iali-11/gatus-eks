69a0ff6d81b13293e6295197ec8286dc8329e4e9
iali-11 <ibrahima_11@outlook.com>
2026-09-22 21:34:14 +0100

    feat: sg variable values

---
+++ infra/modules/sg/variable.tf
@@ -0,0 +1,15 @@
+variable "project-name" {
+  type = string
+}
+
+variable "cluster-name" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "default_cidr_block" {
+  type = string
+}
